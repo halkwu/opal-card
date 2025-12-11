63b6d34880486227021104b4eff1a5a9a58714f5
FiGhTHalK <wushichu2000@gmail.com>
2025-12-11 15:47:30 +1100

    Remove file

--- version/index copy.ts
+++
@@ -1,8 +0,0 @@
-import { DateRange, login } from './scraper';
-
-(async () => {
-    const { username, password, startDate, endDate } = await DateRange();
-    const context = await login(username, password);
-
-    // startDate and endDate are ready to be used for transactions scraping
-})();
